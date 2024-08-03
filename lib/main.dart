2edf3dab5d3b45f91a7b70ca3b373c526c19094a
Beyza Çiftaslan <72284155+beyzaciftaslan@users.noreply.github.com>
2024-08-03 12:59:35 +0300

    splash screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:tr_guide/deneme.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:tr_guide/other_screens/auth_page.dart';
 import 'package:tr_guide/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Future.delayed(const Duration(seconds: 5)); //cok mu uzun oldu
+
+  FlutterNativeSplash.remove();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
