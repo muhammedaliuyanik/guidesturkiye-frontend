8ed1376e43981f0d47db00964fab9895b4064609
Beyza Çiftaslan <72284155+beyzaciftaslan@users.noreply.github.com>
2024-08-20 17:36:53 +0300

    Settings screen
    settings screen created w/ logout button ((mostly ui

---
+++ lib/presentation/screens/settings_screen.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tr_guide/core/providers/user_provider.dart';
+import 'package:tr_guide/presentation/screens/auth_page.dart';
+import 'package:tr_guide/presentation/screens/login_screen.dart';
+import 'package:tr_guide/services/auth_service.dart';
+import 'package:tr_guide/utils/colors.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool isDarkMode =
+      false;
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).getUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Settings',
+          style: TextStyle(color: redColor),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: redColor),
+          //profile screen
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            // User Profile Section
+            Column(
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(user!.photoUrl),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  user.name,
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  user.email,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    await AuthService().signOut();
+                    print('User signed out successfully');
+                    Navigator.push(
+                      // ignore: use_build_context_synchronously
+                      context,
+                      MaterialPageRoute(builder: (context) => const AuthPage()),
+                    );
+                    
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: redColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                  ),
+                  child: const Text('Logout',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            // 
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              color: Colors.grey[200],
+              width: double.infinity,
+              child: const Text(
+                'Application Settings',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.brightness_6),
+              title: const Text('Mode'),
+              subtitle: const Text('Light / Dark'),
+              trailing: Switch(
+                value: isDarkMode,
+                onChanged: (bool value) {
+                  setState(() {
+                    isDarkMode = value;
+                  });
+                },
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.language),
+              title: Text('Language'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+            const SizedBox(height: 20),
+            // info
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              color: Colors.grey[200],
+              width: double.infinity,
+              child: const Text(
+                'Information',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(Icons.info),
+              title: Text('About App'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+            const ListTile(
+              leading: Icon(Icons.article),
+              title: Text('Terms & Conditions'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+            const ListTile(
+              leading: Icon(Icons.privacy_tip),
+              title: Text('Privacy Policy'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+            const ListTile(
+              leading: Icon(Icons.share),
+              title: Text('Share This App'),
+              trailing: Icon(Icons.arrow_forward_ios),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
