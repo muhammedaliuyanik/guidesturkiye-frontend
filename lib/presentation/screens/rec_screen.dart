a2d627d304c6c6f187a08cdca11f40bbcce3a902
Beyza Çiftaslan <72284155+beyzaciftaslan@users.noreply.github.com>
2024-08-27 12:07:34 +0300

    Update rec_screen.dart
    new UI for the rec screen

--- lib/presentation/screens/rec_screen.dart
+++ lib/presentation/screens/rec_screen.dart
@@ -16,15 +16,7 @@ class _RecScreenState extends State<RecScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        centerTitle: true,
-        title: const Text(
-          "Which city do you travel?",
-          style: TextStyle(color: Colors.brown, fontSize: 20),
-        ),
-      ),
+      
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(16.0),
@@ -34,15 +26,15 @@ class _RecScreenState extends State<RecScreen> {
               // Lottie Animation
               Lottie.asset(
                 'assets/animations/Animation - 1724154553547.json', // Replace with your actual Lottie file path
-                height: 200,
+                height: 150,
               ),
               const SizedBox(height: 20),
               const Text(
-                "Please select the city you plan to visit before we suggest locations. Based on your selection, we will recommend places in that city.",
+                "Which city are you in now?",
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.black54, fontSize: 18),
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 16),
               // Dropdown Button
               Container(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -75,6 +67,44 @@ class _RecScreenState extends State<RecScreen> {
                 ),
               ),
               const SizedBox(height: 40),
+              // Dropdown Button 2
+              const Text(
+                "Please select the city you plan to visit before we suggest locations. Based on your selection, we will recommend places in that city.",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black54, fontSize: 18),
+              ),
+              const SizedBox(height: 16),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 10,
+                    ),
+                  ],
+                ),
+                child: DropdownButton<String>(
+                  isExpanded: true,
+                  value: selectedCity,
+                  icon: const Icon(Icons.arrow_drop_down, color: Colors.black),
+                  underline: const SizedBox(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedCity = newValue!;
+                    });
+                  },
+                  items: cities.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value, style: const TextStyle(fontSize: 16)),
+                    );
+                  }).toList(),
+                ),
+              ),
+              const SizedBox(height: 25),
               // Action Button
               ElevatedButton(
                 onPressed: () {
